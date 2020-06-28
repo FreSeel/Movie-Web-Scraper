ba9a79b8877389509e6a9cb0f196c2ca65918471
Frederico Seelow <frederico.seelow@gmail.com>
2020-06-28 12:44:32 +0200

    Implement random function

--- parsedata.py
+++ parsedata.py
@@ -1,5 +1,12 @@
 import json
+import random
 
+#Reads and parses the Data of the file movieData.json
 with open('movieData.json') as json_data:
     jsonData = json.load(json_data)
 
+#Picks a random movie from the list
+def pick_random():
+    length = len(jsonData)
+    rnd_index = random.randint(0,length)
+    print(jsonData[rnd_index])
